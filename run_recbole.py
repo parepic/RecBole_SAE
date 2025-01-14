29637a8d430b13956280d0f386019cbee4fc9d00
Parviz <P.ahmadov@student.tudelft.nl>
2025-01-14 16:11:18 +0100

    test

--- run_recbole.py
+++ run_recbole.py
@@ -53,6 +53,9 @@ if __name__ == "__main__":
 
     parameter_dict = {
         'train_neg_sample_args': None,
+        'sae_k': 8,
+        'sae_scale_size': 32,
+        'sae_lr':1e-3
     }   
     
     # run(
@@ -68,31 +71,31 @@ if __name__ == "__main__":
     # )
     
     
-    config, model, dataset, train_data, valid_data, test_data = load_data_and_model(
-        model_file='./recbole/saved/SASRec-Jan-12-2025_16-43-54.pth',
-    )  # Here you can replace it by your model path.
-
     # config, model, dataset, train_data, valid_data, test_data = load_data_and_model(
-    #     model_file='./saved/SASRec-Jan-13-2025_20-40-37.pth', sae=True
+    #     model_file='./recbole/saved/SASRec-Jan-12-2025_16-43-54.pth',
     # )  # Here you can replace it by your model path.
+
+    config, model, dataset, train_data, valid_data, test_data = load_data_and_model(
+        model_file='./saved/SASRec_SAE-Jan-14-2025_14-54-50.pth', sae=True
+    )  # Here you can replace it by your model path.
 
     trainer = get_trainer(config["MODEL_TYPE"], config["model"])(config, model)
 
 
-    trainer.fit_SAE(config, 
-                    './recbole/saved/SASRec-Jan-12-2025_16-43-54.pth',
-                    train_data,
-                    dataset,
-                    valid_data=valid_data,
-                    show_progress=True
-                    )
+    # trainer.fit_SAE(config, 
+    #                 './recbole/saved/SASRec-Jan-12-2025_16-43-54.pth',
+    #                 train_data,
+    #                 dataset,
+    #                 valid_data=valid_data,
+    #                 show_progress=True
+    #                 )
                     
     # test_result = trainer.evaluate(
     #     test_data, model_file='./saved/SASRec-Jan-12-2025_16-43-54.pth', show_progress=config["show_progress"] 
     # )
     
-    # test_result = trainer.evaluate(
-    #     test_data, model_file='./saved/SASRec-Jan-13-2025_20-40-37.pth', show_progress=config["show_progress"], SAE = True, config=config, dataset=dataset 
-    # )
+    test_result = trainer.evaluate(
+        test_data, model_file='./saved/SASRec_SAE-Jan-14-2025_14-54-50.pth', show_progress=config["show_progress"]
+    )
 
     # print(test_result)
