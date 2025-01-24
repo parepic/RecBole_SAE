cd14e2eb233adb6413818b2a83f110d0ad30c686
Parviz <P.ahmadov@student.tudelft.nl>
2025-01-24 21:26:09 +0100

    bug fix

--- recbole/model/sequential_recommender/sasrec_sae.py
+++ recbole/model/sequential_recommender/sasrec_sae.py
@@ -62,12 +62,12 @@ class SASRec_SAE(SASRec):
     def full_sort_predict(self, interaction):
         item_seq = interaction[self.ITEM_SEQ]
         item_seq_len = interaction[self.ITEM_SEQ_LEN]
-        user_ids = interaction['user_id']
         seq_output = self.forward(item_seq, item_seq_len)
         test_items_emb = self.item_embedding.weight
         scores = torch.matmul(seq_output, test_items_emb.transpose(0, 1))  # [B n_items]
         top_recs = torch.argsort(scores, dim=1, descending=True)[:, :10]
         if(self.mode == "test"):
+            user_ids = interaction['user_id']
             self.sae_module.update_highest_activations(item_seq, top_recs, user_ids)
         for key in top_recs.flatten():
             self.recommendation_count[key.item()] += 1
