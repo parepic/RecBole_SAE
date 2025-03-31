cfb62d8d01fbdeb0df86d66c97c9debe2dbfc456
Parviz <P.ahmadov@student.tudelft.nl>
2025-03-31 14:28:46 +0200

    Up-to-date version

--- recbole/model/sequential_recommender/gate.py
+++ recbole/model/sequential_recommender/gate.py
@@ -75,8 +75,7 @@ class AdaptiveGating(nn.Module):
         gate_values = self.gate_mlp(hidden)  # [batch_size, 3]
         gated_hidden = hidden.clone()
         for i, idx in enumerate(self.gate_indices):
-            gated_hidden[:, idx] += gate_values[:, i]
-            gated_hidden[:, idx] -= gate_values[:, i]
+            gated_hidden[:, idx] += (gate_values[:, i] -  gate_values[:, i])
         # print(f"Gate values: {gate_values[0]} | Gate hidden: {gated_hidden[0]} | Indices: {self.gate_indices[0]}")
                 # print(f"Gate values: {gate_values[0]} | Gate hidden: {gated_hidden[0]} | Indices: {self.gate_indices[0]}")
         print('gate values ', gate_values[0])
