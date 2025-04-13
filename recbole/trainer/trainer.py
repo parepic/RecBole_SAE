f4c57506639b528d179d4985b1225721a29e1c5b
Parviz <P.ahmadov@student.tudelft.nl>
2025-04-13 22:17:54 +0200

    training sasrec sae

--- recbole/trainer/trainer.py
+++ recbole/trainer/trainer.py
@@ -758,11 +758,11 @@ class Trainer(AbstractTrainer):
         # message_output = "Loading model structure and parameters from {}".format(
         #     checkpoint_file
         # )
-        # self.logger.info(message_output)
-        # sasrec_sae = SASRec_SAE(config, dataset, sasrec_model_path=checkpoint_file)
-        # self.model = sasrec_sae
-        # config["model"] = "SASRec_SAE"
-        # self.optimizer = torch.optim.Adam(self.model.sae_module.parameters(), lr=config['sae_lr'])
+        self.logger.info(message_output)
+        sasrec_sae = SASRec_SAE(config, dataset, sasrec_model_path=checkpoint_file)
+        self.model = sasrec_sae
+        config["model"] = "SASRec_SAE"
+        self.optimizer = torch.optim.Adam(self.model.sae_module.parameters(), lr=config['sae_lr'])
 
         message_output = "Loading SASREC model structure and parameters from {}".format(
             checkpoint_file
