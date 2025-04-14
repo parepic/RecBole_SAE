87ef89c05a3ece097153cc9f7f2d3cc254b3f20f
Parviz <P.ahmadov@student.tudelft.nl>
2025-04-14 17:54:40 +0200

    training sasrec sae

--- recbole/quick_start/quick_start.py
+++ recbole/quick_start/quick_start.py
@@ -253,7 +253,7 @@ def load_data_and_model(model_file, device='cuda', sae=True):
     config = checkpoint["config"]
     config["sae_lr"] = 1e-4
     config["sae_k"] = 4
-    config["sae_scale_size"] = 128
+    config["sae_scale_size"] = 256
 
     config["valid_metric"] = 'NDCG@10'
     config["eval_batch_size"] = 1024
