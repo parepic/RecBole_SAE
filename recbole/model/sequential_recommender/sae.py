52e716dc432394595c69bf0e14e72217e97d228c
Parviz <P.ahmadov@student.tudelft.nl>
2025-04-14 18:05:32 +0200

    training sasrec sae

--- recbole/model/sequential_recommender/sae.py
+++ recbole/model/sequential_recommender/sae.py
@@ -232,7 +232,7 @@ class SAE(nn.Module):
 				return x_reconstructed
 			num_dead = self.hidden_dim - len(self.previous_activate_latents)
 			print("num dead ", num_dead)
-			k_aux = int(x.shape[-1] / 2)
+			k_aux = int(x.shape[-1] * 16)
 			if num_dead == 0:
 				self.auxk_loss = 0.0
 				return x_reconstructed
