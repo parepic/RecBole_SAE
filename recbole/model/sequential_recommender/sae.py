f1cff18272a3c25ef9b256475c199fa59ac7283b
Parviz <P.ahmadov@student.tudelft.nl>
2025-04-19 18:55:46 +0200

    training SAE k64

--- recbole/model/sequential_recommender/sae.py
+++ recbole/model/sequential_recommender/sae.py
@@ -110,7 +110,7 @@ class SAE(nn.Module):
 
 	def topk_activation(self, x, sequences, save_result):
 		topk_values, topk_indices = torch.topk(x, self.k, dim=1)
-		y_idx = pd.read_csv(r"./dataset/ml-1m/row_stats_popular.csv")["index"].tolist()
+		y_idx = pd.read_csv(r"./dataset/ml-1m/row_stats_popular.csv")["index"]
 
 		# slice out only the allowed columns
 		x_sub = x.index_select(1, y_idx)                             # shape (B, M)
