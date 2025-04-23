a1fee5b9f502380b2cd319ad455a767abe3d1407
Parviz <P.ahmadov@student.tudelft.nl>
2025-04-23 17:44:52 +0200

    beauty test

--- recbole/utils/utils.py
+++ recbole/utils/utils.py
@@ -1342,7 +1342,7 @@ def get_popularity_label_indices(id_tensor):
                       each item in id_tensor.
     """
     # Read the CSV that maps item IDs to popularity labels.
-    df = pd.read_csv(r"./dataset/Amazon_Beauty/item_popularity_labels_with_titles.csv", encoding='latin1')
+    df = pd.read_csv(r"./dataset/steam/item_popularity_labels_with_titles.csv", encoding='latin1')
     
     # Create a mapping from item ID to popularity label.
     id_to_label = dict(zip(df['item_id:token'], df['popularity_label']))
