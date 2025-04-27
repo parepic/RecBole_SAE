655ba0ee91f9ef9d97cce8643a258a91ae88180e
Parviz <P.ahmadov@student.tudelft.nl>
2025-04-27 06:00:12 +0200

    mind test

--- recbole/quick_start/quick_start.py
+++ recbole/quick_start/quick_start.py
@@ -256,7 +256,7 @@ def load_data_and_model(model_file, device='cuda', sae=True):
     config["sae_lr"] = 5e-4
 
     config["valid_metric"] = 'NDCG@10'
-    config["eval_batch_size"] = 24
+    config["eval_batch_size"] = 2048
     if(device == 'cpu'):
         config.internal_config_dict['use_gpu'] = False
         config.internal_config_dict['gpu_id'] = '-1'
