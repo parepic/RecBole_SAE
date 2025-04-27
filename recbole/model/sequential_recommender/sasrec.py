62276fc5e09f86745256c768b4a3c6d5faecd7ec
Parviz <P.ahmadov@student.tudelft.nl>
2025-04-27 05:43:24 +0200

    mind test

--- recbole/model/sequential_recommender/sasrec.py
+++ recbole/model/sequential_recommender/sasrec.py
@@ -509,13 +509,13 @@ class SASRec(SequentialRecommender):
         # save_batch_activations(seq_output, 64)
         test_items_emb = self.item_embedding.weight
         scores = torch.matmul(seq_output, test_items_emb.transpose(0, 1))  # [B n_items]
-        # scores[:, 0] =  float("-inf")
+        scores[:, 0] =  float("-inf")
         # print(scores[:, 0:20])
         # scores = torch.tensor(self.simple_reranker(scores)).to(self.device)
         # scores = self.FAIR(scores).to(self.device)
         # scores = self.pct_rerank(scores=scores, user_interest=item_seq)
         # scores = self.random_reranker(scores=scores, top_k=20)
-        scores = fair_rerank_exact(torch.sigmoid(scores), alpha=0.1)
+        # scores = fair_rerank_exact(torch.sigmoid(scores), alpha=0.1)
         top_recs = torch.argsort(scores, dim=1, descending=True)[:, :10]
         for key in top_recs.flatten():
             self.recommendation_count[key.item()] += 1
