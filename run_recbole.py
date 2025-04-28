6fbb141cbef7d029216cdff9126bf0701fc489da
Parviz <P.ahmadov@student.tudelft.nl>
2025-04-28 03:57:56 +0200

    ml-1m test

--- run_recbole.py
+++ run_recbole.py
@@ -387,12 +387,12 @@ def create_visualizations_neurons():
     for change in tochange:
         if count==0:
             test_result = trainer.evaluate(
-                valid_data, model_file=args.path, show_progress=config["show_progress"]
+                test_data, model_file=args.path, show_progress=config["show_progress"]
             )
             print(test_result) 
         else:
             test_result = trainer.evaluate(
-                valid_data, model_file=args.path, show_progress=config["show_progress"], N=4096, beta=1.5
+                test_data, model_file=args.path, show_progress=config["show_progress"], N=4096, beta=1.5
             )
         count += 1
         ndcgs.append(test_result['ndcg@10'])
