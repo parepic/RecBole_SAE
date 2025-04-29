a9454ae4863caf9525bfdb694e14298bdd79c455
Parviz <P.ahmadov@student.tudelft.nl>
2025-04-29 17:22:13 +0200

    ml-1m test

--- run_recbole.py
+++ run_recbole.py
@@ -765,7 +765,7 @@ def tune_hyperparam_pmmf():
     
 
     # 2) build your grid
-    all_Ns   = [1e6]
+    all_Ns   = [0]
     betas   = [5e-1, 5e-2, 5e-3]
 
     # 3) baseline & bookkeeping (updated values)
