d463bce03ed660b8dfdc0c2fc7b099be7c52b1b8
Parviz <P.ahmadov@student.tudelft.nl>
2025-05-01 04:20:05 +0200

    sasrec sae train

--- recbole/model/sequential_recommender/sae.py
+++ recbole/model/sequential_recommender/sae.py
@@ -205,8 +205,8 @@ class SAE(nn.Module):
 		combined_sorted = sorted(combined_neurons, key=lambda x: abs(x[1]), reverse=True)
 		top_neurons = combined_sorted[:int(self.N)]
 		# Load the corresponding statistics files.
-		stats_unpop = pd.read_csv(r"./dataset/ml-1m/row_stats_popular.csv")
-		stats_pop = pd.read_csv(r"./dataset/ml-1m/row_stats_unpopular.csv")
+		stats_unpop = pd.read_csv(r"./dataset/lastfm/row_stats_popular.csv")
+		stats_pop = pd.read_csv(r"./dataset/lastfm/row_stats_unpopular.csv")
 
 		# Create tensors of the absolute Cohen's d values for the selected neurons.
 		abs_cohens = torch.tensor([abs(c) for _, c, _ in top_neurons], device=pre_acts.device)
