523c1e6df9f32b4b674c03cbce8607cf610deac0
Parviz <P.ahmadov@student.tudelft.nl>
2025-05-01 05:45:19 +0200

    sasrec sae train

--- recbole/model/sequential_recommender/sae.py
+++ recbole/model/sequential_recommender/sae.py
@@ -205,8 +205,8 @@ class SAE(nn.Module):
 		combined_sorted = sorted(combined_neurons, key=lambda x: abs(x[1]), reverse=True)
 		top_neurons = combined_sorted[:int(self.N)]
 		# Load the corresponding statistics files.
-		stats_unpop = pd.read_csv(r"./dataset/ml-1m/row_stats_popular.csv")
-		stats_pop = pd.read_csv(r"./dataset/ml-1m/row_stats_unpopular.csv")
+		stats_unpop = pd.read_csv(r"./dataset/lastfm/row_stats_popular.csv")
+		stats_pop = pd.read_csv(r"./dataset/lastfm/row_stats_unpopular.csv")
 
 		# Create tensors of the absolute Cohen's d values for the selected neurons.
 		abs_cohens = torch.tensor([abs(c) for _, c, _ in top_neurons], device=pre_acts.device)
@@ -285,8 +285,8 @@ class SAE(nn.Module):
 		pre_acts = self.encoder(sae_in)
 		self.last_activations = pre_acts
 		if self.corr_file:
-			pre_acts = self.dampen_neurons(pre_acts)
-			# pre_acts = self.add_noise(pre_acts, std=self.beta)
+			# pre_acts = self.dampen_neurons(pre_acts)
+			pre_acts = self.add_noise(pre_acts, std=self.beta)
 		pre_acts = nn.functional.relu(pre_acts)   
 		z = self.topk_activation(pre_acts, sequences, save_result=False)
 
